958ebf8c597173f3df393869df7a4d48e85526dc
Bastian12321 <Basclemmensen@gmail.com>
2025-01-10 14:46:55 +0100

    Progress - Game (unfinished bet function)

--- src/main/java/dk/dtu/Server/Poker.java
+++ src/main/java/dk/dtu/Server/Poker.java
@@ -1,51 +1,89 @@
 package dk.dtu.Server;
 
+import dk.dtu.Common.Card;
+import org.jspace.ActualField;
 import org.jspace.RemoteSpace;
+import org.jspace.SpaceRepository;
 
 import java.io.IOException;
-import java.net.URI;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Poker implements Runnable {
     private String uri;
     private RemoteSpace space;
-    private RemoteSpace player1;
-    private RemoteSpace player2;
-    private ArrayList<Card> deck = new ArrayList<>();
+    private ArrayList<RemoteSpace> playerSpaces = new ArrayList<>();
+    private ArrayList<RemoteSpace> activePlayerSpaces = playerSpaces;
+    private ArrayList<Player> players = new ArrayList<>();
+    private ArrayList<Player> activePlayers = players;
+    private int pool;
+    private ShuffledDeck deck;
+    private Card[] cardsInPlay;
+    private int highestBet;
 
     public Poker(String uri) throws IOException {
         this.space = new RemoteSpace(uri + "/gameState?conn");
         this.uri = uri;
+        this.pool = 0;
     }
 
-    public void initializeDeck() {
-        Suite[] suites = {Suite.HEARTS, Suite.DIAMONDS, Suite.CLUBS, Suite.SPADES};
-        for (Suite suite : suites) {
-            for (int i = 2; i <= 14; i++) {
-                deck.add(new Card(i, suite));
-            }
-        }
-        Collections.shuffle(deck);
+    public void bet() {
+
     }
 
     public void setBlinds() {
+        Player first = players.get(0);
+        RemoteSpace firstSpace = playerSpaces.get(0);
+        first.makeBet(10);
+        pool += 10;
+        players.get(1).makeBet(20);
+        pool += 20;
+        players.remove(0);
+        players.add(players.size(), first);
+        playerSpaces.remove(0);
+        playerSpaces.add(players.size(), firstSpace);
+        highestBet = 20;
+    }
 
+    public boolean checkRound() {
+        activePlayers.removeIf(p -> p.getStatus().equals("Fold"));
+        for (Player p : activePlayers) {
+            if (highestBet < p.getBet()) {
+                highestBet = p.getBet();
+            }
+        }
+
+        for (Player p : activePlayers) {
+            if (highestBet != p.getBet()) {
+                if (!p.getStatus().equals("All in")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void dealCards() throws Exception {
+        deck = new ShuffledDeck();
+        for (int i = 0; i < players.size(); i++) {
+            Card[] cards = deck.deal();
+            players.get(i).setHand(cards);
+            playerSpaces.get(i).put("Hand", cards);
+        }
     }
 
     public void startGame() throws Exception {
-        Deck deck = new Deck();
-        this.player1 = new RemoteSpace(uri + "/player1?conn");
-        this.player2 = new RemoteSpace(uri + "/player2?conn");
+        players.add(new Player("Christian", 10000, "/player1"));
+        players.add(new Player("Shannon", 1000000, "/player2"));
+        playerSpaces.add(new RemoteSpace(uri + players.get(0).getUriPart() + "?conn"));
+        playerSpaces.add(new RemoteSpace(uri + players.get(1).getUriPart() + "?conn"));
 
-        Card card1 = deck.drawCard();
-        System.out.println(card1.toString());
-        card1 = deck.drawCard();
-        System.out.println(card1.toString());
-        card1 = deck.drawCard();
-        System.out.println(card1.toString());
-        card1 = deck.drawCard();
-        System.out.println(card1.toString());
+        setBlinds();
+        dealCards();
+        cardsInPlay = deck.flop();
+
+        space.put("Flop", deck.flop());
+
+
     }
     @Override
     public void run() {
--- src/main/java/dk/dtu/Server/ShuffledDeck.java
+++ src/main/java/dk/dtu/Server/ShuffledDeck.java
@@ -1,13 +1,16 @@
 package dk.dtu.Server;
 
+import dk.dtu.Common.Card;
+import dk.dtu.Common.Suite;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Deck {
+public class ShuffledDeck {
 
     private ArrayList<Card> cards = new ArrayList<>();
 
-    public Deck() {
+    public ShuffledDeck() {
         Suite[] suites = {Suite.HEARTS, Suite.DIAMONDS, Suite.CLUBS, Suite.SPADES};
         for (Suite suite : suites) {
             for (int i = 2; i <= 14; i++) {
@@ -24,6 +27,19 @@ public class Deck {
         Card top = cards.get(0);
         cards.remove(0);
         return top;
+    }
+
+    public Card[] deal() throws Exception {
+        return new Card[]{drawCard(), drawCard()};
+    }
+
+    public Card[] flop() throws Exception {
+        return new Card[]{
+                drawCard(),
+                drawCard(),
+                drawCard(),
+                drawCard(),
+                drawCard()};
     }
 
     public int getSize() {
