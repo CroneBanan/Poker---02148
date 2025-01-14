1e4d47b8e98bb9205c3cd6a42b32e5c7c4dc0d7c
Chris523432 <chrisldao@gmail.com>
2025-01-14 10:52:32 +0100

    Display

---
+++ src/main/java/dk/dtu/Client/Display.java
@@ -0,0 +1,17 @@
+package dk.dtu.Client;
+
+import java.util.ArrayList;
+
+public class Display {
+    ArrayList<PlayerInfo> playerNames;
+
+    public ArrayList<String> displayGameState() {
+        ArrayList<String> playerName = new ArrayList<>();
+        for (PlayerInfo player: playerNames) {
+
+        }
+        return playerName;
+    }
+
+
+}
